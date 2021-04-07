7bbd59e1a06ac1d1e606688d18e72504df125147
krudrangi <krudrangi@rentpath.com>
2021-04-07 17:05:54 -0400

    tweaks

--- tests/shared/pdp.js
+++ tests/shared/pdp.js
@@ -33,6 +33,7 @@ test('F1-t1 Verify URL on PDP', async t => {
 test('F1-t2 Verify & Validate PDP Listing Title', async t => {
   await t
     .expect(pdpPage.propertyTitle.exists).ok()
+    .expect(pdpPage.propertyTitle.visible).ok()
     .expect((pdpPage.propertyTitle).innerText).eql(pdpListingTitle, '** Listing Name NOT matching **');
 });
 
@@ -41,7 +42,7 @@ test('F1-t3 First Inline Lead submit on PDP', async t => {
     .setTestSpeed(0.5)
     .expect(pdpPage.leadFormXpathSelector.nth(0).exists).ok()
     .typeText(pdpPage.leadFormNameXpathSelector.nth(0), 'FOODY DAN', { speed: 0.5 })
-    .typeText(pdpPage.leadFormEmailXpathSelector.nth(0), 'kiran@testcafe.com', { speed: 0.5 })
+    .typeText(pdpPage.leadFormEmailXpathSelector.nth(0), 'kiran@testcafe.com', { replace: true, paste: true })
     .typeText(pdpPage.leadFormPhoneXpathSelector.nth(0), '8885554444', { replace: true, paste: true })
     .expect((pdpPage.leadFormSubmitButtonXpathSelector.nth(0)).innerText).eql('Send', '** Send Button text NOT matching **');
 
@@ -50,11 +51,11 @@ test('F1-t3 First Inline Lead submit on PDP', async t => {
     .click(pdpPage.leadFormSubmitButtonXpathSelector.nth(0));
 
   await t
-    .expect(pdpPage.thankyouModalAckMsg.exists).ok()
+    .expect(pdpPage.thankyouModalAckMsg.visible).ok()
     .expect((pdpPage.thankyouModalAckMsg).innerText).eql('Your message has been sent.', '** Ack Msg NOT matching **')
-    .expect(pdpPage.thankyouModalCloseBtn.exists).ok()
+    .expect(pdpPage.thankyouModalCloseBtn.visible).ok()
     .click(pdpPage.thankyouModalCloseBtn)
-    .expect(pdpPage.leadFormXpathSelector.nth(0).exists).ok();
+    .expect(pdpPage.leadFormXpathSelector.nth(0).visible).ok();
 });
 
 // test('F1-t4 Validate Thank you Modal', async t => {
