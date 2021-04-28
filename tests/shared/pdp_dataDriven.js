2ea6a35f093c35cd086b4e64917e7506e442055b
krudrangi <krudrangi@rentpath.com>
2021-04-28 16:10:53 -0400

    DataDriven+DetectClientPlatform

--- tests/shared/pdp_dataDriven.js
+++ tests/shared/pdp_dataDriven.js
@@ -10,18 +10,43 @@ const pdpListingTitle = 'The Savoy';
 fixture('F1 - PDP PAGE FIXTURE')
 
 dataSet.forEach(data => {
-  console.log(data)
   test.page(pdpUrl)
     .requestHooks(authorize)
-    (`SENDING LEAD for - ${data.name}`, async t => {
+    (`LEAD test for - ${data.name}`, async t => {
+      let i = 0;
+      if (t.browser.platform === 'desktop') {
+        i = 2;
+        console.log('Running test on ', t.browser.platform + ' ' + t.browser.name)
+      } else if (t.browser.platform === 'mobile') {
+        i = 1;
+        console.log('Running test on ', t.browser.platform + ' ' + t.browser.name)
+      } else if (t.browser.platform === 'tablet') {
+        i = 1;
+        console.log('Running test on ', t.browser.platform + ' ' + t.browser.name)
+      }
       await t
         .maximizeWindow()
         .setTestSpeed(0.5)
-        .expect(pdpPage.leadFormXpathSelector.nth(1).visible).ok()
-        .typeText(pdpPage.leadFormNameXpathSelector.nth(1), data.name, { speed: 0.5 })
-        .typeText(pdpPage.leadFormEmailXpathSelector.nth(1), data.email, { replace: true, paste: true })
-        .typeText(pdpPage.leadFormPhoneXpathSelector.nth(1), data.phone, { replace: true, paste: true })
-        .expect((pdpPage.leadFormSubmitButtonXpathSelector.nth(1)).innerText).eql('Send', '** Send Button text NOT matching **');
+        .expect(pdpPage.leadFormXpathSelector.nth(i).visible).ok()
+        .hover(pdpPage.leadFormSubmitButtonXpathSelector.nth(i))
+        .typeText(pdpPage.leadFormNameXpathSelector.nth(i), data.name, { replace: true, paste: true })
+        .typeText(pdpPage.leadFormEmailXpathSelector.nth(i), data.email, { replace: true, paste: true })
+        .typeText(pdpPage.leadFormPhoneXpathSelector.nth(i), data.phone, { replace: true, paste: true })
+        .expect((pdpPage.leadFormSubmitButtonXpathSelector.nth(i)).innerText).eql('Send', '** Send Button text NOT matching **');
+
+      await t
+        .expect(pdpPage.leadFormSubmitButtonXpathSelector.nth(i).exists).ok()
+        .click(pdpPage.leadFormSubmitButtonXpathSelector.nth(i));
+
+      await t
+        .expect(pdpPage.thankyouModalAckMsg.visible).ok()
+        .expect((pdpPage.thankyouModalAckMsg).innerText).eql('Your message has been sent.', '** Ack Msg NOT matching **')
+        .expect(pdpPage.thankyouModalCloseBtn.visible).ok()
+        .click(pdpPage.thankyouModalCloseBtn)
+        .expect(pdpPage.leadFormXpathSelector.nth(i).visible).ok();
+      console.log(`PDP Sticky Lead submission completed for ${data.name}`);
+
+      await t.wait(1000);
 
     })
 })
